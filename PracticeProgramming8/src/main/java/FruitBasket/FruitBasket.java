c35ce0f279beb7b5b959a3c9fd19f1ce57311d85
Chris Logsdon <JCLogsdon34@gmail.com>
2017-05-03 20:55:57 -0400

    Catching up almost there

--- PracticeProgramming8/src/main/java/FruitBasket/FruitBasket.java
+++ PracticeProgramming8/src/main/java/FruitBasket/FruitBasket.java
@@ -5,48 +5,49 @@
  */
 package FruitBasket;
 
-import java.util.Scanner;
+
 public class FruitBasket {
-    public static void main(String[] args){
-         String[] fruit = {"Orange", "Apple", "Orange", "Apple", "Orange", 
-             "Apple", "Orange", "Apple", "Orange", "Orange", "Orange", 
-             "Apple", "Orange", "Orange", "Apple", "Orange", "Orange", "Apple", 
-             "Apple", "Orange", "Apple", "Apple", "Orange", "Orange", "Apple", 
-             "Apple", "Apple", "Apple", "Orange", "Orange", "Apple", "Apple",
-             "Orange", "Orange", "Orange", "Orange", "Apple", "Apple", "Apple", 
-             "Apple", "Orange", "Orange", "Apple", "Orange", "Orange", "Apple", 
-             "Orange", "Orange", "Apple", "Apple", "Orange", "Orange", "Apple",
-             "Orange", "Apple", "Orange", "Apple", "Orange", "Apple", "Orange",
-             "Orange"};
 
-         // Scanner fruity = new Scanner();
+    public static void main(String[] args) {
+        String[] fruit = {"Orange", "Apple", "Orange", "Apple", "Orange",
+            "Apple", "Orange", "Apple", "Orange", "Orange", "Orange",
+            "Apple", "Orange", "Orange", "Apple", "Orange", "Orange", "Apple",
+            "Apple", "Orange", "Apple", "Apple", "Orange", "Orange", "Apple",
+            "Apple", "Apple", "Apple", "Orange", "Orange", "Apple", "Apple",
+            "Orange", "Orange", "Orange", "Orange", "Apple", "Apple", "Apple",
+            "Apple", "Orange", "Orange", "Apple", "Orange", "Orange", "Apple",
+            "Orange", "Orange", "Apple", "Apple", "Orange", "Orange", "Apple",
+            "Orange", "Apple", "Orange", "Apple", "Orange", "Apple", "Orange",
+            "Orange"};
+
+        // Scanner fruity = new Scanner();
         // i = fruity.nextLine();
-         
-
-             for (int i = 0; i < 61; i++) {
-                 for(int j =0; j < 61; j++){
-
-              if (Apples[j])> Orange[j + 1]{
-                    temp = wholeNumbers[j];
-                    wholeNumbers[j] = wholeNumbers[j + 1];
-                    wholeNumbers[j + 1] = temp;
-                 }
-                 }
-            }
-             }
-
-             
-                 String[] Apple = new Apple{};
+        int numberOfApples =0;
+        int numberOfOranges =0;
         
-                 String[] Orange = new Orange{};
-         
-         
-         
+        String[] apple = new String[fruit.length];
 
-            int sum +=
-         
+        String[] orange = new String[fruit.length];
+        for (int i = 0; i < fruit.length; i++) {
+            //loop through the fruit basket one time and put them in their appropriate arrays.
+            if(fruit[i].equals("Orange")){
+                orange[i] = "Orange";    
+            }else if(fruit[fruit.length].equals("Apples")){
+           
+                apple[i] = "Apples";
+            }
+            }
+//loop through each array and count number of apples and oranges
+        int sum = numberOfApples + numberOfOranges; 
+        int apples = numberOfApples;
+        int oranges = numberOfOranges;
+        
             System.out.println("The Sum of this array is " + sum);
-         }
-         }
+              System.out.println("The number of apples in this array is " 
+                      + apples);
+                System.out.println("The oranges of this array is " 
+                        + oranges);
     }
 }
+
+
