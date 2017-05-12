197041d78b7859f62562693a2b2328bc89011915
Chris Logsdon <JCLogsdon34@gmail.com>
2017-05-11 21:29:08 -0400

    SummativeSums

---
+++ DogGenetics.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.doggenetics;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class DogGenetics {
+
+    public static void main(String[] args) {
+        String dogName;
+
+        Scanner inputReader = new Scanner(System.in);
+
+        System.out.println("Hello! Would you mind giving us your dog's name?");
+        dogName = inputReader.nextLine();
+
+        System.out.println("Well, I have this reliable DNA report"
+                + " about " + dogName + "'s very dignified background.");
+
+        Random dogGenes = new Random();
+
+        int Dachshund = dogGenes.nextInt(101);
+        int Corgi = dogGenes.nextInt(101 - Dachshund);
+        int Dalmation = dogGenes.nextInt(101 - Dachshund - Corgi);
+        int Wolfhound = dogGenes.nextInt(101 - Dachshund - Corgi - Dalmation);
+        int Mastiff = dogGenes.nextInt(101 - Dachshund - Corgi
+                - Dalmation - Wolfhound);
+
+        System.out.println("It turns out, " + dogName + " is a percent of"
+                + " each of the following breeds: ");
+        System.out.println(Dachshund + "% Dachsund");
+        System.out.println(Corgi + "% Corgi");
+        System.out.println(Dalmation + "% Dalmation");
+        System.out.println(Wolfhound + "% Wolfhound");
+        System.out.println(Mastiff + "% Mastiff");
+
+        System.out.println(" How Distinguished! ");
+    }
+}
---
+++ summativeSums.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.summativesums;
+
+public class summativeSums {
+
+    public static void main(String[] args) {
+
+        int[] firstArray = {1, 90, -33, -55, 67, -16, 28, -55, 15};
+        int[] secondArray = {999, -60, -77, 14, 160, 301};
+        int[] thirdArray = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130,
+            140, 150, 160, 170, 180, 190, 200, -99};
+
+        System.out.println("The sum of the First Array is: " + arraySumm(firstArray));
+        System.out.println("The sum of the Second Array is: " + arraySumm(secondArray));
+        System.out.println("The sum of the Third Array is: " + arraySumm(thirdArray));
+    }
+
+    static int arraySumm(int[] anArray) {
+
+        int summAdder = 0;
+        for (int i = 0; i < anArray.length; i++) {
+            summAdder += anArray[i];
+        }
+        return summAdder;
+    }
+}
