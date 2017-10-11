1a2c92d2a2ae47dd90b2cf83a25dcfd155846068
Chris Logsdon <JCLogsdon34@gmail.com>
2017-10-11 12:17:15 -0400

    VM Noon Work

--- VendingMachine/src/main/java/com/sg/vendingmachine/service/Change.java
+++ VendingMachine/src/main/java/com/sg/vendingmachine/service/Change.java
@@ -35,58 +35,57 @@ public class Change {
     public Map<Coins, Integer> getCoinWorth(BigDecimal itemRefund) {
         List<Coins> denomination = Arrays.asList(Coins.values());
         
-        
-        
+        Map<Coins, Integer> cashMoney = new HashMap<>();
+        cashMoney.put(QUARTER, 25);
+        cashMoney.put(DIME, 10);
+        cashMoney.put(NICKEL, 5);
+        cashMoney.put(PENNY, 1);
+
         BigDecimal quartersOut;
         BigDecimal dimesOut;
         BigDecimal nickelsOut;
         BigDecimal penniesOut;
-        BigDecimal changeOut;
 
         BigDecimal quarters = new BigDecimal(25);
         BigDecimal dimes = new BigDecimal(10);
         BigDecimal nickels = new BigDecimal(5);
         BigDecimal pennies = new BigDecimal(1);
 
-        Map<Coins, Integer> cashMoney = new HashMap<>();
-        cashMoney.put(QUARTER, 25);
-        cashMoney.put(DIME, 10);
-        cashMoney.put(NICKEL, 5);
-        cashMoney.put(PENNY, 1); 
+        quartersOut = itemRefund.remainder(quarters);
+        itemRefund = itemRefund.subtract(quartersOut);
+
+        dimesOut = itemRefund.remainder(dimes);
+        itemRefund = itemRefund.subtract(dimesOut);
+
+        nickelsOut = itemRefund.remainder(nickels);
+        itemRefund = itemRefund.subtract(nickelsOut);
+
+        penniesOut = itemRefund.remainder(pennies);
+        itemRefund = itemRefund.subtract(penniesOut);
 
         for (Coins x : denomination) {
-
-                quartersOut = itemRefund.remainder(quarters);
-                itemRefund = itemRefund.subtract(quartersOut);
-            
-                dimesOut = itemRefund.remainder(dimes); 
-                itemRefund = itemRefund.subtract(dimesOut);
-                
-                nickelsOut = itemRefund.remainder(nickels);
-                itemRefund = itemRefund.subtract(nickelsOut);
-                
-                penniesOut = itemRefund.remainder(pennies);
-                itemRefund = itemRefund.subtract(penniesOut);
-            
-            cashMoney.put(x, cashMoney.get(x) + 1);
-            getCoins(x, 0);
+            BigDecimal coinage = new BigDecimal(x.valueInPennies);
+            do {
+                cashMoney.put(x, cashMoney.get(x) + 1);
+                getCoins(x, 0);
+            } while (itemRefund.compareTo(coinage) > -1);
         }
         return cashMoney;
     }
 
-    public BigDecimal getCashInfo(BigDecimal itemPaidBig, BigDecimal itemPriceBig) 
+    public BigDecimal getCashInfo(BigDecimal itemPaidBig, BigDecimal itemPriceBig)
             throws VendingMachineInsufficientFundsException {
 
         BigDecimal itemRefund;
-        
+
         if (itemPaidBig.compareTo(itemPriceBig) < 0) {
             throw new VendingMachineInsufficientFundsException(
                     "ERROR: Could not vend.  Money"
                     + itemPaidBig
                     + " paid was not sufficient");
-        }else {
+        } else {
             itemRefund = itemPaidBig.subtract(itemPriceBig);
         }
-        return itemRefund; 
+        return itemRefund;
     }
 }
